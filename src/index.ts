c035b3510f1bec4cdda345079ae24c95db2d135d
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-11-06 23:05:59 -0300

    tslint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
-import Koa from 'koa';
+import * as Koa from 'koa'
+import { connect, connection, model, Schema } from 'mongoose'
 
-const app = new Koa();
+const mongodbUri = 'mongodb://localhost:27017/test'
+
+const options = {
+  useMongoClient: true,
+  socketTimeoutMS: 0,
+  keepAlive: true,
+  reconnectTries: 30
+}
+
+const db = connect(mongodbUri, options)
+
+const thingSchema = new Schema({
+  name: String
+})
+const Thing = model('Thing', thingSchema)
+const thing = new Thing({ name: 'test' })
+thing.save() // iAmNotInTheSchema is not saved to the db
+
+const app = new Koa()
 
 // response
 app.use(ctx => {
-  ctx.body = 'Hello Koa';
-});
+  ctx.body = 'Hello Koa'
+})
 
-app.listen(3000);
+app.listen(3000)
