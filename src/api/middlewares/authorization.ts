1d957a86f0f4927f21c92897169bf5d2b068e6bb
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-11-09 13:11:55 -0300

    create middleware authorization

---
+++ src/api/middlewares/authorization.ts
@@ -0,0 +1,26 @@
+import { verify } from 'jsonwebtoken'
+import { usersController } from '../../modules/Users/User'
+import { Vault } from '../../modules/Vault/Vault'
+
+export const authorization = (urls: string[]) => {
+  return async (ctx: any, next: any) => {
+    try {
+      const secret = await Vault.readSecret('poet')
+      const { jwt } = secret.data
+      const url = ctx.url.replace('/', '')
+      const notRequiredToken = urls.includes(url)
+
+      if (notRequiredToken) return next()
+
+      const { token } = ctx.request.body
+      const decoded = verify(token, jwt)
+      const { client_token, email } = decoded as any
+
+      await Vault.verifyToken(client_token)
+      ctx.state.user = await usersController.get(email)
+      await next()
+    } catch (e) {
+      ctx.status = 401
+    }
+  }
+}
