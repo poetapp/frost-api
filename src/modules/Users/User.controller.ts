03bde070f57301947ec4b64ad0d73519dd68ec8f
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-11-09 17:46:16 -0300

    remove async await

--- src/modules/Users/User.controller.ts
+++ src/modules/Users/User.controller.ts
@@ -8,19 +8,19 @@ export class UsersController {
     this.dao = dao
   }
 
-  public async create(user: User) {
+  public create(user: User) {
     return this.dao.create(user)
   }
 
-  public async get(email: string): Promise<User> {
+  public get(email: string): Promise<User> {
     return this.dao.get(email)
   }
 
-  public async update(id: string, user: User) {
+  public update(id: string, user: User) {
     return this.dao.update(id, user)
   }
 
-  public async delete(id: string) {
+  public delete(id: string) {
     return this.dao.delete(id)
   }
 }
