be783d8e085107d3654dfeb3f673f57138f0e471
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-11-09 13:26:08 -0300

    get secret with vault

--- src/api/accounts/accounts.ts
+++ src/api/accounts/accounts.ts
@@ -3,16 +3,16 @@ import { Argon2 } from '../../modules/Argon2/Argon2'
 import { usersController } from '../../modules/Users/User'
 import { Vault } from '../../modules/Vault/Vault'
 
-const SECRET = 'poet'
-
 const createJwtToken = (data: object, secret: string, expiresIn: number) => {
   return sign(data, secret, { expiresIn })
 }
 
 const getToken = async (email: string) => {
+  const secret = await Vault.readSecret('poet')
+  const { jwt } = secret.data
   const tokenVault = await Vault.createToken({ policies: ['goldfish'] })
   const { client_token, lease_duration } = tokenVault.auth
-  return createJwtToken({ email, client_token }, SECRET, lease_duration)
+  return createJwtToken({ email, client_token }, jwt, lease_duration)
 }
 
 export const createAccount = async (ctx: any, next: any) => {
@@ -34,7 +34,9 @@ export const loginAccount = async (ctx: any, next: any) => {
     const user = ctx.request.body
     const response = await usersController.get(user.email)
     const argon2 = new Argon2()
+
     await argon2.verify(user.password, response.password)
+
     const token = await getToken(user.email)
     ctx.body = { token }
   } catch (e) {
