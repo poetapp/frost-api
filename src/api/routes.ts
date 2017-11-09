b287ba66d44902661c45fda4fddaefb06d6321cc
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-11-09 13:12:48 -0300

    use middleware authorization

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,8 +1,12 @@
 import * as KoaRouter from 'koa-router'
 import { createAccount, loginAccount } from './accounts/accounts'
+import { authorization } from './middlewares/authorization'
 import { createWork } from './works/works'
 
 const router = new KoaRouter()
+const omitPaths = ['users', 'login']
+
+router.use(authorization(omitPaths))
 
 router.post('/users', createAccount)
 router.post('/login', loginAccount)
