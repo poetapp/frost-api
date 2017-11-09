ea1fd61b9d689c0c4bf1c0fc5eed15e2379e728d
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-11-09 13:13:31 -0300

    remove logic

--- src/api/works/works.ts
+++ src/api/works/works.ts
@@ -1,21 +1,13 @@
-import { verify } from 'jsonwebtoken'
 import { logger } from '../../modules/Logger/Logger'
-import { usersController } from '../../modules/Users/User'
 import { Vault } from '../../modules/Vault/Vault'
-
-const SECRET = 'poet'
 
 export const createWork = async (ctx: any, next: any) => {
   try {
-    const { token } = ctx.request.body
-    const decoded = verify(token, SECRET)
-    const { client_token, email } = decoded as any
-    const result = await Vault.verifyToken(client_token)
-    const user = await usersController.get(email)
+    const { user } = ctx.state
     logger.log('info', `privateKey encypted: ${user.privateKey}`)
     const privateKey = await Vault.decrypt(user.privateKey)
     logger.log('info', `privateKey encypted: ${privateKey}`)
-    ctx.body = {}
+    ctx.status = 200
   } catch (e) {
     ctx.status = 500
   }
