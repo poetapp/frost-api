073eb21e957e6d7f22856b6a27b230b071f24154
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-11-10 18:41:35 -0300

    wip

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -2,18 +2,21 @@ import * as KoaRouter from 'koa-router'
 import {
   createAccount,
   loginAccount,
-  validateAccount
+  recoverAccount,
+  validateAccount,
+  validateRecoverAccount
 } from './accounts/accounts'
 import { authorization } from './middlewares/authorization'
 import { createWork } from './works/works'
 
 const router = new KoaRouter()
-const omitPaths = ['users', 'login']
+const omitPaths = ['users', 'login', 'recover-account', 'home']
 
 router.use(authorization(omitPaths))
 
 router.post('/users', validateAccount, createAccount)
 router.post('/login', validateAccount, loginAccount)
+router.post('/recover-account', validateRecoverAccount, recoverAccount)
 router.post('/work', createWork)
 
 export const routes = router
