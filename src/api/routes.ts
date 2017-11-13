d41283f35b6018a96d3e41bfca3204a61e792184
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-11-13 14:30:20 -0300

    add route

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -3,8 +3,10 @@ import {
   createAccount,
   loginAccount,
   recoverAccount,
+  recoverAccountToken,
   validateAccount,
-  validateRecoverAccount
+  validateRecoverAccount,
+  validateRecoverAccountToken
 } from './accounts/accounts'
 import { authorization } from './middlewares/authorization'
 import { createWork } from './works/works'
@@ -17,6 +19,11 @@ router.use(authorization(omitPaths))
 router.post('/users', validateAccount, createAccount)
 router.post('/login', validateAccount, loginAccount)
 router.post('/recover-account', validateRecoverAccount, recoverAccount)
+router.post(
+  '/recover-account-token',
+  validateRecoverAccountToken,
+  recoverAccountToken
+)
 router.post('/work', createWork)
 
 export const routes = router
