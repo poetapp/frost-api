ce7b3138787a60bfe54cdea261efca0ff7b1c665
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-11-14 18:26:32 -0300

    add new routes

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -8,18 +8,23 @@ import { ChangePassword } from './accounts/changePassword/changePassword'
 import { CreateAccount } from './accounts/create/create'
 import { ForgotPassword } from './accounts/forgotPassword/forgotPassword'
 import { Login } from './accounts/login/login'
+import { VerifyAccount } from './accounts/verify/verify'
+import { VerifyAccountToken } from './accounts/verifyToken/verifyToken'
 import { Work } from './works/works'
 
 const router = new KoaRouter()
 const route = new Route(router)
 
-const omitPaths = [Path.ACCOUNT, Path.LOGIN, Path.PASSWORD_FORGOT]
-
-router.use(authorization(omitPaths))
+router.use(
+  [Path.ACCOUNT_VERIFY_TOKEN, Path.PASSWORD_CHANGE, Path.WORK],
+  authorization()
+)
 
 route.set(Method.POST, Path.ACCOUNT, new CreateAccount())
+route.set(Method.POST, Path.ACCOUNT_VERIFY, new VerifyAccount())
+route.set(Method.GET, Path.ACCOUNT_VERIFY_TOKEN, new VerifyAccountToken())
 route.set(Method.POST, Path.LOGIN, new Login())
-route.set(Method.POST, Path.PASSWORD_FORGOT, new ForgotPassword())
+route.set(Method.POST, Path.PASSWORD_RESET, new ForgotPassword())
 route.set(Method.POST, Path.PASSWORD_CHANGE, new ChangePassword())
 route.set(Method.POST, Path.WORK, new Work())
 
