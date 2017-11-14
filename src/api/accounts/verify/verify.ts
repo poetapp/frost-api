d4f913ed1828c9be07866d7756e582f2937ecb03
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-11-14 18:27:35 -0300

    set url development

---
+++ src/api/accounts/verify/verify.ts
@@ -0,0 +1,35 @@
+import * as Joi from 'joi'
+import { template, subject } from '../../../emails/verify'
+import { Nodemailer } from '../../../modules/Nodemailer/Nodemailer'
+import { getToken } from '../utils/utils'
+
+import { ControllerApi } from '../../../interfaces/ControllerApi'
+
+export class VerifyAccount implements ControllerApi {
+  async handler(ctx: any, next: any): Promise<any> {
+    try {
+      const { email } = ctx.request.body
+      const token = await getToken(email)
+
+      const data = {
+        to: email,
+        from: `"Po.et" <contact@po.et>`,
+        subject,
+        html: template(`http://localhost:3000/account/verify/${token}`)
+      }
+
+      await Nodemailer.sendMail(data)
+      ctx.status = 200
+    } catch (e) {
+      throw e
+    }
+  }
+
+  validate(): object {
+    return {
+      email: Joi.string()
+        .email()
+        .required()
+    }
+  }
+}
