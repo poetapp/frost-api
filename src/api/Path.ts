f821430150671ba353e6e7eff2535d8823bea2d5
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-11-14 18:24:11 -0300

    add new route and improve names

--- src/api/Path.ts
+++ src/api/Path.ts
@@ -1,7 +1,9 @@
 export enum Path {
   ACCOUNT = '/account',
+  ACCOUNT_VERIFY = '/account/verify',
+  ACCOUNT_VERIFY_TOKEN = '/account/verify/:token',
   LOGIN = '/login',
-  PASSWORD_FORGOT = '/password/forgot',
+  PASSWORD_RESET = '/password/reset',
   PASSWORD_CHANGE = '/password/change',
   WORK = '/work'
 }
