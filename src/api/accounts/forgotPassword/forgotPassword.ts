55b7003345cd7e0cab2476a5be9e1e3c346b611b
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-11-14 22:53:44 -0300

    check if exist the user

--- src/api/accounts/forgotPassword/forgotPassword.ts
+++ src/api/accounts/forgotPassword/forgotPassword.ts
@@ -2,25 +2,39 @@ import * as Joi from 'joi'
 import { template, subject } from '../../../emails/forgotPassword'
 import { ControllerApi } from '../../../interfaces/ControllerApi'
 import { Nodemailer } from '../../../modules/Nodemailer/Nodemailer'
+import { usersController } from '../../../modules/Users/User'
+import { errors } from '../../errors/errors'
 import { getToken } from '../utils/utils'
 
 export class ForgotPassword implements ControllerApi {
+  async sendEmail(email: string) {
+    const token = await getToken(email)
+
+    const data = {
+      to: email,
+      from: `"Po.et" <contact@po.et>`,
+      subject,
+      html: template(token)
+    }
+
+    await Nodemailer.sendMail(data)
+  }
+
   async handler(ctx: any, next: any): Promise<any> {
     try {
       const { email } = ctx.request.body
-      const token = await getToken(email)
-
-      const data = {
-        to: email,
-        from: `"Po.et" <contact@po.et>`,
-        subject,
-        html: template(token)
+      const user = await usersController.get(email)
+      const { ResourceNotFound } = errors
+      if (user) {
+        await this.sendEmail(email)
+        ctx.status = 200
+      } else {
+        ctx.status = ResourceNotFound.code
+        ctx.body = ResourceNotFound.message
       }
-
-      await Nodemailer.sendMail(data)
-      ctx.status = 200
     } catch (e) {
-      throw e
+      const { InternalError } = errors
+      ctx.throw(InternalError.code, InternalError.message)
     }
   }
 
