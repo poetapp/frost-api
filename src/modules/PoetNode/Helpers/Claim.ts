01f0775d44b123fde56bac1b6a22797757cdea89
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-11-23 13:35:01 -0300

    Poet Node, Add files

---
+++ src/modules/PoetNode/Helpers/Claim.ts
@@ -0,0 +1,123 @@
+// tslint:disable
+import * as crypto from 'crypto'
+
+import * as bitcore from 'bitcore-lib'
+
+import { IllegalArgumentException } from '../API/Exceptions'
+import {
+  Claim,
+  ClaimAttributes,
+  ClaimType,
+  Work
+} from '../Interfaces/Interfaces'
+
+import { Serialization } from './Serialization'
+
+export function isClaim(object: any): object is Claim {
+  // TODO: use joi or protobuf
+  return (
+    object.id &&
+    object.publicKey &&
+    object.signature &&
+    object.type &&
+    object.attributes
+  )
+}
+
+export function isWork(claim: Claim): claim is Work {
+  return claim.type === ClaimType.Work
+}
+
+export function getClaimId(claim: Claim): string {
+  const buffer = Buffer.from(
+    Serialization.claimToHex({
+      ...claim,
+      id: '',
+      signature: ''
+    }),
+    'hex'
+  )
+  return crypto
+    .createHash('sha256')
+    .update(buffer)
+    .digest()
+    .toString('hex')
+}
+
+export function getClaimSignature(claim: Claim, privateKey: string): string {
+  if (!claim.publicKey)
+    throw new IllegalArgumentException(
+      'Cannot sign a claim that has an empty .publicKey field.'
+    )
+  if (
+    new bitcore.PrivateKey(privateKey).publicKey.toString() !== claim.publicKey
+  )
+    throw new IllegalArgumentException(
+      "Cannot sign this claim with the provided privateKey. It doesn\t match the claim's public key."
+    )
+  if (!claim.id)
+    throw new IllegalArgumentException(
+      'Cannot sign a claim that has an empty .id field.'
+    )
+  if (claim.id !== getClaimId(claim))
+    throw new IllegalArgumentException(
+      'Cannot sign a claim whose id has been altered or generated incorrectly.'
+    )
+
+  const signature = bitcore.crypto.ECDSA.sign(
+    Buffer.from(claim.id, 'hex'),
+    bitcore.PrivateKey(privateKey)
+  )
+  return signature.toString()
+}
+
+export function isValidSignature(claim: Claim): boolean {
+  try {
+    return bitcore.crypto.ECDSA.verify(
+      Buffer.from(claim.id, 'hex'),
+      bitcore.crypto.Signature.fromString(claim.signature),
+      new bitcore.PublicKey(claim.publicKey)
+    )
+  } catch (exception) {
+    console.log(
+      JSON.stringify(
+        {
+          message: 'Exception caught while attempting to verify signature.',
+          claim,
+          exception
+        },
+        null,
+        2
+      )
+    )
+    return false
+  }
+}
+
+export function createClaim(
+  privateKey: string,
+  type: ClaimType,
+  attributes: ClaimAttributes
+): Claim {
+  const claim: Claim = {
+    id: '',
+    publicKey: new bitcore.PrivateKey(privateKey).publicKey.toString(),
+    signature: '',
+    type,
+    dateCreated: new Date(),
+    attributes
+  }
+  const id = getClaimId(claim)
+  const signature = getClaimSignature(
+    {
+      ...claim,
+      id
+    },
+    privateKey
+  )
+  return {
+    ...claim,
+    id,
+    signature
+  }
+}
---
+++ typings/bitcore-lib.d.ts
@@ -0,0 +1,136 @@
+// tslint:disable
+declare module 'bitcore-lib' {
+  export = bitcoreLib
+}
+
+declare namespace bitcoreLib {
+  export const Block: Block
+  export const Transaction: Transaction
+  export const util: Util
+  export const Script: Script
+  export const PrivateKey: PrivateKey
+
+  export namespace crypto {
+    class BN {}
+    namespace ECDSA {
+      function sign(message: Buffer, key: PrivateKey): Signature
+      function verify(
+        hashbuf: Buffer,
+        sig: Signature,
+        pubkey: PublicKey,
+        endian?: 'little'
+      ): boolean
+    }
+    export namespace Hash {
+      function sha256(buffer: Buffer): Uint8Array
+    }
+    export const Random: RandomInterface
+    export const Point: PointInterface
+
+    class Signature {
+      static fromDER(sig: Buffer): Signature
+      static fromString(data: string): Signature
+      SIGHASH_ALL: number
+      toString(): string
+    }
+  }
+
+  interface Transaction {
+    inputs: Input[]
+    outputs: Output[]
+    readonly id: string
+    readonly hash: string
+    nid: string
+
+    new (serialized?: any): this
+    (serialized?: any): this
+
+    from(utxos: UTXO[]): Transaction
+    to(address: Address | string, amount: number): Transaction
+    change(address: Address | string): Transaction
+    sign(privateKey: PrivateKey | string): Transaction
+    applySignature(sig: crypto.Signature): Transaction
+    addData(data: Buffer): this
+  }
+
+  interface Block {
+    hash: string
+    transactions: any[]
+    header: {
+      time: number
+      prevHash: string
+    }
+
+    new (data: Buffer | object): Block
+    (data: Buffer | object): Block
+  }
+
+  interface PrivateKey {
+    readonly publicKey: PublicKey
+
+    new (key: string): this
+    (source: string): this
+  }
+
+  class PublicKey {
+    constructor(source: string)
+  }
+
+  interface Output {
+    readonly script: any
+  }
+
+  interface Script {
+    types: {
+      DATA_OUT: string
+    }
+    buildPublicKeyHashOut(address: Address): Script
+  }
+
+  interface Util {
+    readonly buffer: {
+      reverse(a: any): any
+    }
+  }
+
+  interface UnspentOutput {
+    readonly address: Address
+    readonly txId: string
+    readonly outputIndex: number
+    readonly script: Script
+    readonly satoshis: number
+
+    (data: object): this
+    new (data: object): this
+
+    inspect(): string
+    fromObject(o: object): this
+    toObject(): this
+  }
+
+  export namespace Networks {
+    interface Network {
+      readonly name: string
+      readonly alias: string
+    }
+
+    export const livenet: Network
+    export const mainnet: Network
+    export const testnet: Network
+
+    export function add(data: any): Network
+    export function remove(network: Network): void
+    export function get(
+      args: string | number | Network,
+      keys: string | string[]
+    ): Network
+  }
+}
+
+interface Address {}
+
+interface Input {}
+
+interface UTXO {}
+interface RandomInterface {}
+interface PointInterface {}
