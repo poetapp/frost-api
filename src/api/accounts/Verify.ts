3de2d01c02a53ffa5a08d7c5121e289306d9f683
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-12-15 14:24:42 -0300

    Fix, verify account, gets URL of the configuration

--- src/api/accounts/Verify.ts
+++ src/api/accounts/Verify.ts
@@ -3,19 +3,23 @@ import { template, subject } from '../../emails/verify'
 import { Nodemailer } from '../../utils/Nodemailer/Nodemailer'
 import { getToken } from './utils/utils'
 
+import { configuration } from '../../configuration'
 import { ControllerApi } from '../../interfaces/ControllerApi'
+import { Path } from '../Path'
 
 export class VerifyAccount implements ControllerApi {
   async handler(ctx: any, next: any): Promise<any> {
     try {
       const { email } = ctx.request.body
       const token = await getToken(email)
+      const { ACCOUNTS_VERIFY } = Path
+      const { frostUrl } = configuration
 
       const data = {
         to: email,
         from: `"Po.et" <contact@po.et>`,
         subject,
-        html: template(`http://localhost:3000/account/verify/${token}`)
+        html: template(`${frostUrl}${ACCOUNTS_VERIFY}/${token}`)
       }
 
       await Nodemailer.sendMail(data)
