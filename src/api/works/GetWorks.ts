cce14100c75455890d05bdac47a64ccba216001e
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-12-15 15:17:48 -0300

    Add controller for the endpoint get all works

---
+++ src/api/works/GetWorks.ts
@@ -0,0 +1,31 @@
+import { errors } from '../../errors/errors'
+import { ControllerApi } from '../../interfaces/ControllerApi'
+import { WorksController } from '../../modules/Works/Works'
+import { WorkAttributes } from '../../utils/PoetNode/Interfaces/Interfaces'
+
+export class GetWorks implements ControllerApi {
+  async handler(ctx: any, next: any): Promise<any> {
+    try {
+      const { user } = ctx.state
+      const { publicKey } = user
+
+      const worksController = new WorksController()
+      try {
+        const response = await worksController.getWorksByPublicKey(publicKey)
+
+        ctx.body = response.map((work: WorkAttributes) => work.attributes)
+        return
+      } catch (e) {
+        const { WorkNotFound } = errors
+        ctx.status = WorkNotFound.code
+        ctx.body = WorkNotFound.message
+      }
+    } catch (e) {
+      ctx.status = 500
+    }
+  }
+
+  validate() {
+    return {}
+  }
+}
