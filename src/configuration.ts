771b3158d067bb91cedc3b9b94cbf1643a25afd5
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-12-15 14:26:42 -0300

    Add path in the configuration

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,8 +1,9 @@
-const { MONGODB_URL, VAULT_URL, VAULT_TOKEN, POET_URL } = process.env
+const { MONGODB_URL, VAULT_URL, VAULT_TOKEN, POET_URL, FROST_URL } = process.env
 
 export const configuration = {
-  mongodbUrl: MONGODB_URL || 'mongodb://localhost:27017/frost',
-  vaultUrl: VAULT_URL || 'http://localhost:8200',
   vaultToken: VAULT_TOKEN || 'frost',
-  poetUrl: POET_URL || 'http://localhost:18080'
+  vaultUrl: VAULT_URL || 'http://localhost:8200',
+  mongodbUrl: MONGODB_URL || 'mongodb://localhost:27017/frost',
+  poetUrl: POET_URL || 'http://localhost:18080',
+  frostUrl: FROST_URL || 'http://localhost:3000'
 }
