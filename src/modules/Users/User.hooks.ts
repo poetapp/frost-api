6b90591c600e6a2e9b33adfc99a08e412c8739ae
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-12-15 14:50:00 -0300

    Save public key

--- src/modules/Users/User.hooks.ts
+++ src/modules/Users/User.hooks.ts
@@ -2,16 +2,21 @@ const { PrivateKey } = require('bitcore-lib')
 import { Argon2 } from '../../utils/Argon2/Argon2'
 import { Vault } from '../../utils/Vault/Vault'
 
-const createPrivateKey = (): string => {
-  const privateKey = new PrivateKey()
-  return privateKey.toWIF()
+const createKeys = (): { privateKey: string; publicKey: string } => {
+  const key = new PrivateKey()
+  const privateKey = key.toWIF()
+  const publicKey = new PrivateKey(privateKey).publicKey.toString()
+  return { privateKey, publicKey }
 }
 
 export const validate = async function(next: any) {
+  const { privateKey, publicKey } = createKeys()
+
   const argon2 = new Argon2(this.password)
   this.createdAt = Date.now()
-  this.privateKey = await Vault.encrypt(createPrivateKey())
+  this.privateKey = await Vault.encrypt(privateKey)
   this.password = await argon2.hash()
   this.verified = false
+  this.publicKey = publicKey
   next()
 }
