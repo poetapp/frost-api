91e83803ded58594dee5d0c0ebd6b51fdbcdff61
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-12-20 18:53:25 -0300

    init vault, unseal vault and set secrets

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,16 @@ import { MongoDB } from './databases/mongodb/mongodb'
 import { logger } from './utils/Logger/Logger'
 import { Nodemailer } from './utils/Nodemailer/Nodemailer'
 import { Vault } from './utils/Vault/Vault'
+const Box = require('cli-box')
+require('console.table')
 
-const { mongodbUrl, vaultUrl, vaultToken } = configuration
+const {
+  mongodbUrl,
+  vaultUrl,
+  vaultToken,
+  transactionalMandrill,
+  jwt
+} = configuration
 
 const mongodbUri = mongodbUrl
 
@@ -25,22 +33,82 @@ const optionsVault = {
   apiVersion: 'v1'
 }
 
-const main = async () => {
+const initVault = async () => {
   try {
-    try {
-      Vault.config(optionsVault)
-      const secret = await Vault.readSecret('frost')
-      const optionsNodemailer = {
-        apiKey: secret.data['transactional-mandrill']
-      }
+    const config = await Vault.init()
 
-      Nodemailer.config(optionsNodemailer)
-    } catch (e) {
-      logger.log('Error Vault: ', e)
+    // Box with aligned text to top-right
+    const box = Box('30x5', {
+      text: `
+      \u001b[31mIMPORTANT!!!\u001b[0m
+      \nCopy this keys in a secure place. \nThese keys are very important and it does not way recovery.`,
+      stretch: true,
+      autoEOL: true,
+      vAlign: 'middle',
+      hAlign: 'left'
+    })
+
+    /* tslint:disable:no-console */
+
+    console.log(box)
+    console.table(config)
+
+    /* tslint:enable:no-console */
+
+    const { keys, root_token } = config
+    const vault = Vault.getInstance()
+    vault.token = root_token
+
+    await Vault.unseal(keys[0])
+
+    await Vault.mountTransit()
+    await Vault.writeTransitKey()
+
+    // Secrets
+    const value = {
+      transactionalMandrill,
+      jwt
     }
 
+    await Vault.writeSecret('frost', value)
+    return true
+  } catch (e) {
+    logger.log('error', e.message)
+  }
+}
+
+const main = async () => {
+  try {
+    Vault.config(optionsVault)
+
+    // Init vault and unseal
+    // https://www.vaultproject.io/intro/getting-started/deploy.html
+    if (!vaultToken) await initVault()
+  } catch (e) {
+    logger.log('error', e.message)
+  }
+
+  // Try to read Vault
+  try {
+    const secret = await Vault.readSecret('frost')
+    const optionsNodemailer = {
+      apiKey: secret.data.transactionalMandrill
+    }
+
+    Nodemailer.config(optionsNodemailer)
+  } catch (e) {
+    logger.log('error', e.message)
+  }
+
+  // Connect mongodb
+  try {
     const mongoDB = new MongoDB(mongodbUri, optionsMongoDB)
     await mongoDB.start()
+  } catch (e) {
+    logger.log('error', e.message)
+  }
+
+  try {
     const app = new Koa()
 
     app
@@ -50,7 +118,7 @@ const main = async () => {
 
     app.listen(3000)
   } catch (e) {
-    logger.log('error', e)
+    logger.log('error', e.message)
   }
 }
 
