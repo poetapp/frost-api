eecce8d3b4dec7eb4308583105f2667993b56d57
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-12-20 18:42:57 -0300

    new variables

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,9 +1,11 @@
 const { MONGODB_URL, VAULT_URL, VAULT_TOKEN, POET_URL, FROST_URL } = process.env
 
 export const configuration = {
-  vaultToken: VAULT_TOKEN || 'frost',
+  vaultToken: VAULT_TOKEN || undefined,
   vaultUrl: VAULT_URL || 'http://localhost:8200',
   mongodbUrl: MONGODB_URL || 'mongodb://localhost:27017/frost',
   poetUrl: POET_URL || 'http://localhost:18080',
-  frostUrl: FROST_URL || 'http://localhost:3000'
+  frostUrl: FROST_URL || 'http://localhost:3000',
+  transactionalMandrill: '***REMOVED***',
+  jwt: 'frost'
 }
