acfe373a7fc3956961c803bcd00afb161f636f21
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2017-12-20 18:41:41 -0300

    refactor

--- src/modules/Users/User.ts
+++
@@ -1,5 +0,0 @@
-import { UsersController } from './User.controller'
-import { UsersDAO } from './Users.dao'
-
-const userDAO = new UsersDAO()
-export const usersController = new UsersController(userDAO)
