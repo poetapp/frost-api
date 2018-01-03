a824e9198b234755c15c1089aabb28576a9d6a69
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-01-03 16:32:46 -0300

    remove logger

--- src/api/works/CreateWork.ts
+++ src/api/works/CreateWork.ts
@@ -1,7 +1,6 @@
 import * as Joi from 'joi'
 import { errors } from '../../errors/errors'
 import { ControllerApi } from '../../interfaces/ControllerApi'
-import { logger } from '../../utils/Logger/Logger'
 import { Vault } from '../../utils/Vault/Vault'
 
 import { WorksController } from '../../modules/Works/Works.controller'
@@ -12,11 +11,7 @@ export class CreateWork implements ControllerApi {
       const { user } = ctx.state
       const { WorkError } = errors
       const newWork = ctx.request.body
-
-      logger.log('info', `privateKey encypted: ${user.privateKey}`)
-
       const privateKey = await Vault.decrypt(user.privateKey)
-
       const work = new WorksController(privateKey, newWork)
       const claim = work.generateClaim()
 
@@ -27,8 +22,6 @@ export class CreateWork implements ControllerApi {
         ctx.body = WorkError.message
         return
       }
-
-      logger.log('info', `privateKey decrypted: ${privateKey}`)
 
       ctx.status = 200
       ctx.body = { workId: claim.id }
