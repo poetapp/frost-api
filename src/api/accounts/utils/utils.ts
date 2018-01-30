9cb41dba553eadf847483cbfdf8a4102374937f0
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-01-30 16:13:24 -0300

    modify getToken

--- src/api/accounts/utils/utils.ts
+++ src/api/accounts/utils/utils.ts
@@ -1,14 +1,15 @@
 import { sign } from 'jsonwebtoken'
 import { Vault } from '../../../utils/Vault/Vault'
+import { TokenOptions } from '../../Tokens'
 
 const createJwtToken = (data: object, secret: string, expiresIn: number) => {
   return sign(data, secret, { expiresIn })
 }
 
-export const getToken = async (email: string, policies: string[]) => {
+export const getToken = async (email: string, options: TokenOptions) => {
   const secret = await Vault.readSecret('frost')
   const { jwt } = secret.data
-  const tokenVault = await Vault.createToken({ policies })
+  const tokenVault = await Vault.createToken(options)
   const { client_token, lease_duration } = tokenVault.auth
   return createJwtToken({ email, client_token }, jwt, lease_duration)
 }
