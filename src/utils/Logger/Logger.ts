00122c8614b03e7dddb0e3fa9286ada00acfb434
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-01-31 13:26:16 -0300

    improve logger

--- src/utils/Logger/Logger.ts
+++ src/utils/Logger/Logger.ts
@@ -1,14 +1,13 @@
-import * as winston from 'winston'
+const { createLogger, format, transports } = require('winston')
+const { combine, timestamp, splat, simple } = format
 
-winston.configure({
-  level: 'info',
+export const logger = createLogger({
+  format: combine(timestamp(), simple(), splat()),
   transports: [
     // - Write to all logs with level `info` and below to `combined.log`
     // - Write all logs error (and below) to `error.log`.
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
+    new transports.Console(),
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' })
   ]
 })
-
-export const logger = winston
