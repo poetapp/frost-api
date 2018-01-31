6bb8650695155211ff9a6a6a93638a6ec881d935
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-01-31 19:34:38 -0300

    catch server errors

--- src/utils/Logger/Logger.ts
+++ src/utils/Logger/Logger.ts
@@ -8,7 +8,8 @@ const customFormat = printf((info: any) => {
       level: info.level,
       type: info.type,
       message: info.message,
-      stackTrace: info.stackTrace
+      stackTrace: info.stackTrace,
+      requestError: info.requestError
     },
     null,
     2
@@ -32,6 +33,16 @@ logger.error = function() {
   const args = Array.prototype.slice.call(arguments, 0)
   if (args.length >= 2 && args[1] instanceof Error)
     args[1].stackTrace = args[1].stack
+
+  if (args.length >= 2 && args[1] instanceof Object && args[1].url) {
+    args[1].requestError = {}
+    args[1].requestError.ok = args[1].ok
+    args[1].requestError.status = args[1].status
+    args[1].requestError.statusText = args[1].statusText
+    args[1].requestError.timeout = args[1].timeout
+    args[1].requestError.url = args[1].url
+    args[1].requestError.requestData = args[2]
+  }
 
   return originalLogError.apply(this, args)
 }
