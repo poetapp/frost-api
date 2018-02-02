c10d650343efb4dc295c22df1230c167a8589221
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-02-02 15:38:39 -0300

    fix, remove test error claim

--- src/modules/Works/Works.controller.ts
+++ src/modules/Works/Works.controller.ts
@@ -32,7 +32,7 @@ export class WorksController {
           Accept: 'application/json',
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ ...workAttributes, ...{ signature: '123' } })
+        body: JSON.stringify(workAttributes)
       })
 
       if (createWork.ok) return await createWork.text()
