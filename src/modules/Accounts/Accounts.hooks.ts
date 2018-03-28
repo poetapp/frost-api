29dfa3d867f66066ab9b467762ce2dd3209a46c1
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-03-28 12:47:47 -0300

    Api tokens (#35)

--- src/modules/Accounts/Accounts.hooks.ts
+++ src/modules/Accounts/Accounts.hooks.ts
@@ -11,12 +11,14 @@ const createKeys = (): { privateKey: string; publicKey: string } => {
 }
 
 export const validate = async function(next: any) {
-  const { privateKey, publicKey } = createKeys()
-  const argon2 = new Argon2(this.password)
-  this.createdAt = Date.now()
-  this.privateKey = await Vault.encrypt(privateKey)
-  this.password = await argon2.hash()
-  this.verified = configuration.verifiedAccount
-  this.publicKey = publicKey
+  if (this.isNew) {
+    const { privateKey, publicKey } = createKeys()
+    const argon2 = new Argon2(this.password)
+    this.createdAt = Date.now()
+    this.privateKey = await Vault.encrypt(privateKey)
+    this.password = await argon2.hash()
+    this.verified = configuration.verifiedAccount
+    this.publicKey = publicKey
+  }
   next()
 }
