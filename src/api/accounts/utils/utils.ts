85b27aaeec990607ed10c2c0fcc37a6365a254c0
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-04-04 12:20:08 -0300

    API Tokens, remove expireIn JWT (#44)

--- src/api/accounts/utils/utils.ts
+++ src/api/accounts/utils/utils.ts
@@ -2,14 +2,14 @@ import { sign } from 'jsonwebtoken'
 import { Vault } from '../../../utils/Vault/Vault'
 import { TokenOptions } from '../../Tokens'
 
-const createJwtToken = (data: object, secret: string, expiresIn: number) => {
-  return sign(data, secret, { expiresIn })
+const createJwtToken = (data: object, secret: string) => {
+  return sign(data, secret)
 }
 
 export const getToken = async (email: string, options: TokenOptions) => {
   const secret = await Vault.readSecret('frost')
   const { jwt } = secret.data
   const tokenVault = await Vault.createToken(options)
-  const { client_token, lease_duration } = tokenVault.auth
-  return createJwtToken({ email, client_token }, jwt, lease_duration)
+  const { client_token } = tokenVault.auth
+  return createJwtToken({ email, client_token }, jwt)
 }
