a596843a0766fb257f97c461a3e848333e607c72
warren vosper <warren.vosper@po.et>
2018-07-17 08:37:37 -0600

    fixes case of variable

--- src/utils/Password/isPwned.ts
+++ src/utils/Password/isPwned.ts
@@ -12,7 +12,7 @@ const runPwnedChecker = (root: string) => async (password: string) => await fetc
 export const result = ({ status }: { [key: string]: any }) => Promise.resolve(status === 200)
 const asyncPipe = (...fns: any[]) => (x: any) => fns.reduce(async (y: any, f: any) => f(await y), x)
 
-export const isPwned = async (password: string, root: string = configuration.PWNEDCHECKER_ROOT) => {
+export const isPwned = async (password: string, root: string = configuration.pwnedCheckerRoot) => {
   try {
     return await asyncPipe(runPwnedChecker(root), result)(password)
   } catch (err) {
