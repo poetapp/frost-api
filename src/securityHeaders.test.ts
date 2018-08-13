6eac754e2c2cde10c1b209bbd078705f16f7fc23
warren vosper <straydogsw@gmail.com>
2018-08-13 11:56:56 -0600

    adds security headers unit tests (#245)

---
+++ src/securityHeaders.test.ts
@@ -0,0 +1,33 @@
+import { describe } from 'riteway'
+import { securityHeaders } from './securityHeaders'
+
+describe('Security headers', async (should: any) => {
+  const { assert } = should('')
+
+  {
+    assert({
+      given: 'an object with security header configuration',
+      should: 'have the frameguard action set to "deny"',
+      actual: securityHeaders.frameguard.action,
+      expected: 'deny',
+    })
+  }
+
+  {
+    assert({
+      given: 'an object with security header configuration',
+      should: 'have the referrer policy set to "same-origin"',
+      actual: securityHeaders.referrerPolicy.policy,
+      expected: 'same-origin',
+    })
+  }
+
+  {
+    assert({
+      given: 'an object with security header configuration',
+      should: 'have the content security policy scriptSrc set to "self"',
+      actual: securityHeaders.contentSecurityPolicy.directives.scriptSrc,
+      expected: ["'self'"],
+    })
+  }
+})
