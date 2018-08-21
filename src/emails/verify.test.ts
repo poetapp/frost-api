fd6169a299626dccc5d637865c8dd6b9d8c004c0
warren vosper <straydogsw@gmail.com>
2018-08-21 10:49:33 -0600

    adds unit tests for email templates (#280)

---
+++ src/emails/verify.test.ts
@@ -0,0 +1,16 @@
+import { describe } from 'riteway'
+import { template } from './verify'
+
+describe('verify', async (should: any) => {
+  const { assert } = should()
+  const link = 'the.verify/link/for/passwords'
+  const pattern = new RegExp(`href="${link}"`)
+  const actual = pattern.test(template(link))
+
+  assert({
+    given: 'a template and a link for verifying the user password',
+    should: 'return the link in the template output',
+    actual,
+    expected: true,
+  })
+})
