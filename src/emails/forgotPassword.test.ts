124f949e10c255e4d85f10ffdc5d03ba63bc4ffe
warren vosper <straydogsw@gmail.com>
2018-08-21 10:49:33 -0600

    adds unit tests for email templates (#280)

---
+++ src/emails/forgotPassword.test.ts
@@ -0,0 +1,16 @@
+import { describe } from 'riteway'
+import { template } from './forgotPassword'
+
+describe('forgotPassword', async (should: any) => {
+  const { assert } = should()
+  const link = 'the.reset/link/for/passwords'
+  const pattern = new RegExp(`href="${link}"`)
+  const actual = pattern.test(template(link))
+
+  assert({
+    given: 'a template and a link for resetting the user password',
+    should: 'return the link in the template output',
+    actual,
+    expected: true,
+  })
+})
