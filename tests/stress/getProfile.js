b3ed93febc193a68978722975a774bbdda6b817a
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-11-15 18:46:28 -0300

    test: Stress test Get Profile (#699)

---
+++ tests/stress/getProfile.js
@@ -0,0 +1,29 @@
+import http from 'k6/http'
+import { check, sleep } from 'k6'
+
+const FROST_HOST = __ENV.FROST_HOST || 'http://0.0.0.0:3000'
+
+export const setup = () => {
+  const sufix = Date.now()
+  const payload = JSON.stringify({
+    email: `test${sufix}@po.et`,
+    password: 'aB%12345678910'
+  })
+
+  const url = `${FROST_HOST}/accounts`
+  const params =  { headers: { 'Content-Type': 'application/json' } }
+  const res = http.post(url, payload, params)
+  return res.json()
+}
+
+export default (data) => {
+  const url = `${FROST_HOST}/accounts/profile`
+  const params =  { headers: { 'Content-Type': 'application/json', token: data.token } }
+  const res = http.get(url, params)
+
+  check(res, {
+    'status 200': (r) => r.status === 200
+  })
+
+  sleep(1)
+}
