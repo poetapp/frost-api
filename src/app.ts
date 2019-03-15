df3e37008c34c6309ac6b480339e9c0e34c953fe
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-03-14 23:59:05 -0300

    refactor: app and Vault initialization (#871)

--- src/app.ts
+++ src/app.ts
@@ -1,145 +1,26 @@
-require('dotenv').config({ path: '.env' })
-const Redis = require('ioredis')
-import * as fs from 'fs'
 import * as Pino from 'pino'
-import { promisify } from 'util'
+
+const Redis = require('ioredis')
 
 import { API } from './api/API'
 import { Configuration } from './configuration'
 import { MongoDB } from './databases/mongodb/mongodb'
-import './extensions/Error'
+import { initVault } from './initVault'
 import { loadConfigurationWithDefaults } from './loadConfiguration'
-import { delay } from './utils/Delay/Delay'
-import { createModuleLogger } from './utils/Logging/Logging'
+import { loggingConfigurationToPinoConfiguration } from './utils/Logging/Logging'
 import { Vault } from './utils/Vault/Vault'
-const Box = require('cli-box')
+
+import './extensions/Error'
+
+require('dotenv').config({ path: '.env' })
 require('console.table')
-
-interface InitVault {
-  transactionalMandrill: string
-  jwt: string
-}
-
-const readFile = promisify(fs.readFile)
-const VAULT_FILE = '/tmp/vault.json'
-
-const initVault = async ({ transactionalMandrill, jwt }: InitVault, logger: Pino.Logger) => {
-  try {
-    let initialized = false
-    let sealed = true
-    let standby = true
-    let config
-    let status
-
-    while (!initialized) {
-      try {
-        logger.info('info', 'waiting vault')
-        status = await Vault.status()
-        initialized = status.initialized
-        if (!status.initialized) {
-          config = await Vault.init()
-          logger.info('info', 'Vault is initialized')
-        } else
-          config = JSON.parse(await readFile(VAULT_FILE, 'utf8'))
-      } catch (e) {
-        logger.info('error', e.message)
-        logger.info('info', 'Retry in 5 seconds')
-        await delay(5000)
-      }
-
-      status = await Vault.status()
-      initialized = status.initialized
-    }
-
-    if (config) {
-      const box = Box('30x5', {
-        text: `
-        \u001b[31mIMPORTANT!!!\u001b[0m
-        \nCopy this keys in a secure place. \nThese keys are very important and it does not way recovery.`,
-        stretch: true,
-        autoEOL: true,
-        vAlign: 'middle',
-        hAlign: 'left',
-      })
-
-      /* tslint:disable:no-console */
-
-      console.log(box)
-      console.table(config)
-
-      fs.writeFile(VAULT_FILE, JSON.stringify(config, null, '\t'), err => {
-        if (err) return console.log(err)
-
-        console.log('The file was saved!')
-      })
-
-      /* tslint:enable:no-console */
-
-      const { keys, root_token } = config
-      const vault = Vault.getInstance()
-      vault.token = root_token
-
-      while (sealed)
-        try {
-          status = await Vault.status()
-          sealed = status.sealed
-          if (status.sealed) {
-            logger.info('info', 'vault trying unseal')
-            await Vault.unseal(keys[0])
-            logger.info('info', 'Vault is unseal')
-          }
-        } catch (e) {
-          logger.info('error', e.message)
-          logger.info('info', 'Retry in 5 seconds')
-          await delay(5000)
-        }
-
-      while (standby) {
-        status = await Vault.status()
-        standby = status.standby
-        if (standby) {
-          logger.info('info', 'Vault is in standby')
-          logger.info('info', 'Retry in 5 seconds')
-          await delay(5000)
-        }
-      }
-
-      await Vault.mountTransit()
-      await Vault.writeTransitKey()
-      // Secrets
-      const value = {
-        transactionalMandrill,
-        jwt,
-      }
-
-      await Vault.writeSecret('frost', value)
-      return true
-    } else if (status.initialized)
-      logger.info(
-        'error',
-        `Vault was initialized. Check file ${VAULT_FILE} in the container. \
-         You have to set the environment variable VAULT_TOKEN `,
-      )
-  } catch (e) {
-    logger.info('error', e.message)
-  }
-}
 
 export async function app(localVars: any = {}) {
   const configuration: Configuration = loadConfigurationWithDefaults(localVars)
-
-  const loggingConfiguration = {
-    loggingLevel: configuration.loggingLevel,
-    loggingPretty: configuration.loggingPretty,
-  }
-
-  const logger = createModuleLogger(loggingConfiguration)(__dirname)
+  const logger = Pino(loggingConfigurationToPinoConfiguration(configuration))
 
   logger.info('Running Frost API')
-  logger.info('')
-  logger.info('Loading Configuration...')
-
-  logger.info(configuration)
+  logger.info({ configuration })
 
   const configurationVault = {
     token: configuration.vaultToken,
@@ -147,93 +28,22 @@ export async function app(localVars: any = {}) {
     apiVersion: configuration.vaultApiVersion,
   }
 
-  const configurationInitVault = {
-    transactionalMandrill: configuration.transactionalMandrill,
-    jwt: configuration.jwt,
-  }
-
   let secret
   if (!configuration.skipVault) {
     try {
       Vault.config(configurationVault)
-
-      // Init vault and unseal
-      // https://www.vaultproject.io/intro/getting-started/deploy.html
-      if (!configurationVault.token) await initVault(configurationInitVault, logger)
+      if (!configurationVault.token) await initVault()
       await Vault.mountAuthTune()
     } catch (e) {
-      logger.error(e.message)
+      logger.fatal(e, 'Error with Vault')
+      return
     }
 
     secret = await Vault.readSecret('frost')
   }
 
-  const configurationMongoDB = {
-    mongodbUrl: configuration.mongodbUrl,
-    options: {
-      socketTimeoutMS: configuration.mongodbSocketTimeoutMS,
-      keepAlive: configuration.mongodbKeepAlive,
-      reconnectTries: configuration.mongodbReconnectTries,
-      useNewUrlParser: configuration.mongodbUseNewUrlParser,
-    },
-    loggingConfiguration,
-  }
-
-  const configurationFrostAPI = {
-    host: configuration.frostHost,
-    port: configuration.frostPort,
-    maxApiRequestLimitForm: configuration.maxApiRequestLimitForm,
-    maxApiRequestLimitJson: configuration.maxApiRequestLimitJson,
-    passwordComplex: {
-      min: configuration.passwordComplexMin,
-      max: configuration.passwordComplexMax,
-      lowerCase: configuration.passwordComplexLowerCase,
-      upperCase: configuration.passwordComplexUpperCase,
-      numeric: configuration.passwordComplexNumeric,
-      symbol: configuration.passwordComplexSymbol,
-    },
-    sendEmail: {
-      nodemailer: {
-        mandrill: {
-          apiKey: secret ? secret.data.transactionalMandrill : '',
-        },
-        maildev: {
-          host: configuration.maildevPortTcpAddr,
-          port: configuration.maildevPortTcpPort,
-          ignoreTLS: configuration.maildevIgnoreTLS,
-        },
-        sendEmailDisabled: configuration.sendEmailDisabled,
-        emailTransportMailDev: configuration.emailTransportMailDev,
-      },
-      emailFrom: configuration.emailFrom,
-      emailReply: configuration.emailReply,
-      frostChangePassword: configuration.frostChangePassword,
-      frostVerifiedAccount: configuration.frostVerifiedAccount,
-    },
-    rateLimit: {
-      rateLimitDisabled: configuration.rateLimitDisabled,
-    },
-    limiters: {
-      loginLimiter: {
-        max: configuration.loginRateLimitMax,
-        duration: configuration.loginRateLimitDuration,
-      },
-      accountLimiter: {
-        max: configuration.accountRateLimitMax,
-        duration: configuration.accountRateLimitDuration,
-      },
-      passwordChangeLimiter: {
-        max: configuration.passwordChangeRateLimitMax,
-        duration: configuration.passwordChangeRateLimitDuration,
-      },
-    },
-    poetUrl: configuration.poetUrl,
-    testPoetUrl: configuration.testPoetUrl,
-    maxApiTokens: configuration.maxApiTokens,
-    verifiedAccount: configuration.verifiedAccount,
-    pwnedCheckerRoot: configuration.pwnedCheckerRoot,
-    loggingConfiguration,
-  }
+  const configurationMongoDB = configurationToMongoDB(configuration)
+  const configurationFrostAPI = configurationToFrostAPI(configuration, secret ? secret.data.transactionalMandrill : '')
 
   const redisDB = await new Redis(configuration.redisPort, configuration.redisHost)
   const mongoDB = await MongoDB(configurationMongoDB).start()
@@ -248,3 +58,76 @@ export async function app(localVars: any = {}) {
     },
   }
 }
+
+const configurationToMongoDB = (configuration: Configuration) => ({
+  mongodbUrl: configuration.mongodbUrl,
+  options: {
+    socketTimeoutMS: configuration.mongodbSocketTimeoutMS,
+    keepAlive: configuration.mongodbKeepAlive,
+    reconnectTries: configuration.mongodbReconnectTries,
+    useNewUrlParser: configuration.mongodbUseNewUrlParser,
+  },
+  loggingConfiguration: {
+    loggingLevel: configuration.loggingLevel,
+    loggingPretty: configuration.loggingPretty,
+  },
+})
+
+const configurationToFrostAPI = (configuration: Configuration, mandrillApiKey: string) => ({
+  host: configuration.frostHost,
+  port: configuration.frostPort,
+  maxApiRequestLimitForm: configuration.maxApiRequestLimitForm,
+  maxApiRequestLimitJson: configuration.maxApiRequestLimitJson,
+  passwordComplex: {
+    min: configuration.passwordComplexMin,
+    max: configuration.passwordComplexMax,
+    lowerCase: configuration.passwordComplexLowerCase,
+    upperCase: configuration.passwordComplexUpperCase,
+    numeric: configuration.passwordComplexNumeric,
+    symbol: configuration.passwordComplexSymbol,
+  },
+  sendEmail: {
+    nodemailer: {
+      mandrill: {
+        apiKey: mandrillApiKey,
+      },
+      maildev: {
+        host: configuration.maildevPortTcpAddr,
+        port: configuration.maildevPortTcpPort,
+        ignoreTLS: configuration.maildevIgnoreTLS,
+      },
+      sendEmailDisabled: configuration.sendEmailDisabled,
+      emailTransportMailDev: configuration.emailTransportMailDev,
+    },
+    emailFrom: configuration.emailFrom,
+    emailReply: configuration.emailReply,
+    frostChangePassword: configuration.frostChangePassword,
+    frostVerifiedAccount: configuration.frostVerifiedAccount,
+  },
+  rateLimit: {
+    rateLimitDisabled: configuration.rateLimitDisabled,
+  },
+  limiters: {
+    loginLimiter: {
+      max: configuration.loginRateLimitMax,
+      duration: configuration.loginRateLimitDuration,
+    },
+    accountLimiter: {
+      max: configuration.accountRateLimitMax,
+      duration: configuration.accountRateLimitDuration,
+    },
+    passwordChangeLimiter: {
+      max: configuration.passwordChangeRateLimitMax,
+      duration: configuration.passwordChangeRateLimitDuration,
+    },
+  },
+  poetUrl: configuration.poetUrl,
+  testPoetUrl: configuration.testPoetUrl,
+  maxApiTokens: configuration.maxApiTokens,
+  verifiedAccount: configuration.verifiedAccount,
+  pwnedCheckerRoot: configuration.pwnedCheckerRoot,
+  loggingConfiguration: {
+    loggingLevel: configuration.loggingLevel,
+    loggingPretty: configuration.loggingPretty,
+  },
+})
---
+++ src/initVault.ts
@@ -0,0 +1,100 @@
+import * as fs from 'fs'
+import * as Pino from 'pino'
+import { promisify } from 'util'
+
+import { Configuration } from './configuration'
+import { loadConfigurationWithDefaults } from './loadConfiguration'
+import { delay } from './utils/Delay/Delay'
+import { loggingConfigurationToPinoConfiguration } from './utils/Logging/Logging'
+import { Vault } from './utils/Vault/Vault'
+
+const readFile = promisify(fs.readFile)
+const VAULT_FILE = '/tmp/vault.json'
+
+export const initVault = async () => {
+  const configuration: Configuration = loadConfigurationWithDefaults()
+  const logger = Pino(loggingConfigurationToPinoConfiguration(configuration))
+
+  logger.info('Attempting to initialize Vault.')
+  const config = await initializeWithRetries(logger)
+  logger.info(config, 'Vault configuration obtained.')
+
+  if (!config) {
+    logger.fatal(
+      `Vault was initialized. Check file ${VAULT_FILE} in the container. \
+       You have to set the environment variable VAULT_TOKEN.`,
+    )
+    return
+  }
+
+  const { keys, root_token } = config
+  const vault = Vault.getInstance()
+  vault.token = root_token
+
+  await unsealWithRetries(logger, keys[0])
+  await waitWhileVaultIsInStandby(logger)
+
+  await Vault.mountTransit()
+  await Vault.writeTransitKey()
+
+  const frostSecrets = {
+    transactionalMandrill: configuration.transactionalMandrill,
+    jwt: configuration.jwt,
+  }
+
+  await Vault.writeSecret('frost', frostSecrets)
+}
+
+const initializeWithRetries = async (logger: Pino.Logger) => {
+  let config: any
+  let status: any = null
+
+  while (!status || !status.initialized) {
+    try {
+      status = await Vault.status()
+      if (!status.initialized) {
+        config = await Vault.init()
+        logger.info('Vault initialized successfully.')
+        fs.writeFileSync(VAULT_FILE, JSON.stringify(config, null, '\t'))
+        logger.info({ config, VAULT_FILE }, 'Vault configuration written to disk.')
+      } else {
+        logger.info('Vault was already initialized. Reading Vault configuration from disk.')
+        config = JSON.parse(await readFile(VAULT_FILE, 'utf8'))
+      }
+    } catch (error) {
+      logger.info({ error }, 'Error while initializing Vault. Will retry in 5 seconds.')
+      await delay(5000)
+    }
+
+    status = await Vault.status()
+  }
+
+  return config
+}
+
+const waitWhileVaultIsInStandby = async (logger: Pino.Logger) => {
+  while (true) {
+    const status = await Vault.status()
+    if (!status.standby) return
+    logger.info('Vault is in standby. Will retry in 5 seconds.')
+    await delay(5000)
+  }
+}
+
+const unsealWithRetries = async (logger: Pino.Logger, key: string) => {
+  while (true)
+    try {
+      const status = await Vault.status()
+      if (!status.sealed) {
+        logger.info('Vault was already unsealed.')
+        return
+      }
+      logger.info('Attempting to unseal Vault.')
+      await Vault.unseal(key)
+      logger.info('Vault unsealed successfully.')
+      return
+    } catch (e) {
+      logger.error(e, 'Error unsealing Vault. Will retry in 5 seconds.')
+      await delay(5000)
+    }
+}
